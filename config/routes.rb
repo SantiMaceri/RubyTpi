feab43c4d2429cce26cbf10f82d40bc948c14077
SantiagoMaceri <santimaceri@gmail.com>
2019-01-31 10:37:02 -0300

    small change in routes, helped by Nahuel

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users , only: :create
-  resources :questions do  	
+  resources :questions do 
+  	member do 
+  		put :resolve
+  		end	
   	resources :answers
+
+
   end
 
   resources :sessions, only: :create
-  put 'questions/:id/resolve', to: 'questions#resolve'
+ # put 'questions/:id/resolve', to: 'questions#resolve'
 end
